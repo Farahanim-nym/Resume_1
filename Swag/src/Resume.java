59e7ff7b41397cc5b2eb9b1bd267a7041d4f4d6b
User <User@LAPTOP-RM07FRQ8>
2020-11-02 20:16:18 +0800

    first commit uploading all files

---
+++ Swag/src/Resume.java
@@ -0,0 +1,43 @@
+
+public class Resume {
+
+	public static void main(String[] args) {
+		String name = "Nur Farahanim Binti Mat Amin ";
+		char gender = 'F';
+		String birthday = "11 May 2000";
+		int age = 20;
+		String maritalstatus = "not married";
+		String citizenship = "Malaysia";
+		String address = "67C,Lorong 10/5,Bandar Seri Astana,08000 Sungai Petani Kedah";
+		String hp = "0176058072";
+		String result = "A+";
+		double cgpa = 3.35;
+		String workexperience = "factory operator";
+		String language1 = "Malay (100%)";
+		String language2 = "English (60%)";
+		String skill = "Java Programming(78%)";
+		String awardandachievements = "Malaysia university English Test Band 3"; 
+				
+System.out.println("RESUME");
+System.out.println("Name\t\t: " + name);
+System.out.println("Gender\t\t:" + gender);
+System.out.println("Birthday\t:" + birthday);
+System.out.println("Age\t\t:" + age + " Years Old");
+System.out.println("Marital Status\t:" + maritalstatus);
+System.out.println("Citizenship\t:" +citizenship );
+System.out.println("Home Address\t:" +address );
+System.out.println("HP\t\t:" +hp);
+System.out.println("Exam Result(STIA1113)\t:" +result);
+System.out.println("CGPA\t:" + cgpa);
+System.out.println("Work Experiences\t:"+ workexperience );
+System.out.println();
+System.out.println("Language:");
+System.out.println( "\t\t1." + language1);
+System.out.println("\t\t2." + language2);
+System.out.println();
+System.out.println("Professional Skills\t:" +skill);
+System.out.println();
+System.out.print("Awards and Achievements\t:" +awardandachievements);
+	}
+
+}
